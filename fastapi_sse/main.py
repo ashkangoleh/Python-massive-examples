41fb6aefb92143e7832d17881a017d8381927577
Ashkan GolehPour <a.golehpour@arz.team>
2022-06-27 11:00:40 +0430

    fastapi sse and websocket real time

---
+++ fastapi_sse/main.py
@@ -0,0 +1,55 @@
+import json
+import logging
+import random
+import sys
+from datetime import datetime
+from typing import Iterator
+
+import asyncio
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+random.seed()
+
+
+async def generate_random_data(request: Request) -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    client_ip = request.client.host
+
+    logger.info("Client %s connected", client_ip)
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "value": random.random() * 100,
+            }
+        )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
+
+
+@app.get("/chart-data")
+async def chart_data(request: Request) -> StreamingResponse:
+    response = StreamingResponse(generate_random_data(
+        request), media_type="text/event-stream")
+    # response.headers["Cache-Control"] = "no-cache"
+    # response.headers["X-Accel-Buffering"] = "no"
+    return response
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=7777, reload=True)
