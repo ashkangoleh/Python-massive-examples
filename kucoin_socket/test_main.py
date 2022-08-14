3706ddcf1dc2731141c6f2c25fd7229531449ee6
ashkangoleh <ashkangoleh@gmail.com>
2022-08-14 17:37:03 +0430

    modified:   test_main.py

--- kucoin_socket/test_main.py
+++ kucoin_socket/test_main.py
@@ -3,7 +3,16 @@ import json
 import time
 import unittest
 from main import KucoinWs
-import websocket as ws
+import websockets as ws
+import enum
+
+
+def jsonify(data: dict) -> json:
+    return json.dumps(data)
+
+
+def jsonParser(data):
+    return json.loads(data)
 
 
 def async_run(coroutine):
@@ -17,16 +26,13 @@ def async_run(coroutine):
 
 
 class KucoinWsTest(unittest.TestCase):
-    # ws.enableTrace(True)
-    WS = ws.WebSocket()
     _WS = KucoinWs(topics=['/market/ticker:all',
                            '/market/level2:BTC-USDT'])
 
-    ENDPOINT: str = ''
-    
-    def on_error(self,ws, error):
+
+    def on_error(self, ws, error):
         print("==>> error: ", error)
-        
+
     def test_endpoint_validity(self):
         self._WS.get_ws()
         KucoinWsTest.ENDPOINT = self._WS.wsEndpoint
@@ -36,21 +42,32 @@ class KucoinWsTest(unittest.TestCase):
     async def test_get_id(self):
         await self._WS.get_id()
         self.assertIsNotNone(self._WS.get_id)
-
+        
     @async_run
     async def test_connectivity(self):
         self.test_endpoint_validity()
-        # async with ws.connect(self._WS.wsEndpoint, **self._WS.http_proxy) as websocket:
         try:
-            while 1:
-                channel = ws.WebSocketApp(KucoinWsTest.ENDPOINT,on_error=self.on_error)
-                # channel.run_forever(**self._WS.http_proxy)
-                channel.send({
-                    "id": int(time.time()*1000),
-                    "type": 'ping'
-                })
-                result = ws.recv()
-                print("==>> result: ", result)
-        except ws.WebSocketConnectionClosedException as e:
+            async with ws.connect(KucoinWsTest._WS.wsEndpoint) as channel:
+                self.assertEqual(channel.state.value, 1)
+        except ws.ConnectionClosed as e:
             import traceback
             print(traceback.format_exc())
+            print(f"error: {e}")
+
+    @async_run
+    async def test_ping(self):
+        try:
+            async with ws.connect(KucoinWsTest._WS.wsEndpoint) as channel:
+                await channel.send(jsonify({
+                    "id": int(time.time()*1000),
+                    "type": 'ping'
+                }))
+                result = await channel.recv()
+                result = jsonParser(result)
+
+                self.assertEqual(list(result.keys()), ["id", "type"])
+                self.assertEqual(result['type'], "welcome")
+        except ws.ConnectionClosed as e:
+            import traceback
+            print(traceback.format_exc())
+            print(f"error: {e}")
