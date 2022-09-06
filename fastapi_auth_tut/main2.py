a966497baa9125dc0c96937b02577c492eeebd75
ashkangoleh <ashkangoleh@gmail.com>
2022-09-06 09:42:18 +0430

    fastapi auth_required as decorator

--- fastapi_auth_tut/main2.py
+++ fastapi_auth_tut/main2.py
@@ -1,16 +1,49 @@
-from fastapi import FastAPI, Form, Request, Path
+from functools import wraps
+from typing import Any, Callable, Optional
+from fastapi import FastAPI, Form, Request, Path, Response, Header
 import uvicorn
+from fastapi.responses import JSONResponse
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 app = FastAPI()
 ORIGINS = ['*']
 app.add_middleware(
-            CORSMiddleware,
-            allow_origins=ORIGINS,
-            allow_credentials=True,
-            allow_methods=["*"],
-            allow_headers=["*"],
-        )
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# auth_required decorator
+
+
+def auth_required(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any):
+        try:
+            response = requests.post(
+                url="LOGIN_ENDPOINT",
+                headers={
+                    "Authorization": kwargs["authorization"],
+                },
+            )
+            response.raise_for_status()
+            return func(*args, **kwargs)
+        except requests.HTTPError as error:
+            return Response(
+                status_code=error.response.status_code
+            )
+
+    return wrapper
+
+
+@app.get("/info/")
+@auth_required
+def get_information(
+    authorization: Optional[str] = Header(None)
+) -> JSONResponse:
+    return JSONResponse(content={"info": "The answer is 42."})
 
 
 @app.post('/')
@@ -21,4 +54,4 @@ async def receiveLocationInfo(request: Request):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main2:app',host="localhost",port=7676,reload=True)
+    uvicorn.run('main2:app', host="localhost", port=7676, reload=True)
