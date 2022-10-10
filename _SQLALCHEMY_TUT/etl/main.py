f2e6b605af371a8188a30eae803bf5b99d4bd514
ashkangoleh <ashkangoleh@gmail.com>
2022-10-10 08:25:32 +0330

    new file:   _SQLALCHEMY_TUT/etl/fast.py 	modified:   _SQLALCHEMY_TUT/etl/main.py

--- _SQLALCHEMY_TUT/etl/main.py
+++ _SQLALCHEMY_TUT/etl/main.py
@@ -2,15 +2,15 @@
 from ETL_db import *
 s = sessionmaker(bind=engine)()
 
-users = s.query(Users.UserId,Uploads.UploadId).join(Uploads,Users.UserId==Uploads.UserId,isouter=False).subquery().alias("usr")
-# test = s.query(users).filter(users.c.UserId==1)
+# users = s.query(Users.UserId,Uploads.UploadId).join(Uploads,Users.UserId==Uploads.UserId,isouter=False).subquery().alias("usr")
+# # test = s.query(users).filter(users.c.UserId==1)
 
-a = s.query(Uploads).select_entity_from(users)
-print("==>> a: ", a)
+# a = s.query(Uploads).select_entity_from(users)
+# print("==>> a: ", a)
 
-print("==>> a: ", a.all())
+# print("==>> a: ", a.all())
 
-print("==>> users: ", users)
-# print("==>> test: ", test)
-# print("users",test.all())
+# print("==>> users: ", users)
+# # print("==>> test: ", test)
+# # print("users",test.all())
 
