0c1a1502769dd4fff6cb86302a0b3bbcddad3e57
Ashkan Goleh <ashkangoleh@gmail.com>
2022-12-06 11:34:48 +0330

    modified:   PYTHON_EXTRA/algorithms/binary_search.py 	modified:   PYTHON_EXTRA/algorithms/dijkstra.py 	modified:   PYTHON_EXTRA/algorithms/linear_search.py

--- PYTHON_EXTRA/algorithms/binary_search.py
+++ PYTHON_EXTRA/algorithms/binary_search.py
@@ -1,4 +1,4 @@
-## Binary search only works when your list is in `sorted order`.
+# Binary search only works when your list is in `sorted order`.
 
 def binary_search(orderList, key):
     low = 0
@@ -7,15 +7,11 @@ def binary_search(orderList, key):
 
     while low <= high:
         mid = (low + high) // 2
-
-        print("==>> orderList[mid]: ", orderList[mid])
         if orderList[mid] < key:
             low = mid + 1
-            print("==>> low: ", low)
 
         elif orderList[mid] > key:
             high = mid - 1
-            print("==>> high: ", high)
         else:
             return True, f"index of {key} is {mid}"
     return False, f"{-1} not found ! !"
@@ -24,24 +20,23 @@ def binary_search(orderList, key):
 # print("==>> binary_search([1, 3, 9, 15, 17], 19): ",
 #       binary_search([1, 3, 9, 15, 17], 19))
 print("==>> binary_search([1, 3, 9, 15], 9): ",
-      binary_search([1, 3, 9, 15], 9))
+      binary_search([1, 3, 4, 9, 15, 2], 2))
 
 
-def binarySearch(inlist, key):
-    left = 0
-    right = len(inlist) - 1
-    mid = 0
+# def binarySearch(inlist, key):
+#     left = 0
+#     right = len(inlist) - 1
+#     mid = 0
 
-    while left <= right:
-        mid = (left+right) // 2
+#     while left <= right:
+#         mid = (left+right) // 2
 
-        if inlist[mid] < key:
-            left = mid + 1
-        elif inlist[mid] > key:
-            right = mid - 1
-        else:
-            return True
-    return False
+#         if inlist[mid] < key:
+#             left = mid + 1
+#         elif inlist[mid] > key:
+#             right = mid - 1
+#         else:
+#             return True
+#     return False
 
-print("==>> binary_search([1, 3, 9, 15], 9): ",
-      binarySearch([1, 3, 9, 15], 9))
+# print(binarySearch([5, 3, 9, 15], 12))
--- PYTHON_EXTRA/algorithms/dijkstra.py
+++ PYTHON_EXTRA/algorithms/dijkstra.py
@@ -1,6 +1,3 @@
-# Python program for Dijkstra's single
-# source shortest path algorithm. The program is
-# for adjacency matrix representation of the graph
 class Graph():
  
     def __init__(self, vertices):
