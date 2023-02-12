575f80979ef60dde894301eb020b17b061f9dc8d
Ashkan Goleh <ashkangoleh@gmail.com>
2023-02-12 16:42:30 +0330

    modified:   PYTHON_EXTRA/custom_log.py

--- PYTHON_EXTRA/custom_log.py
+++ PYTHON_EXTRA/custom_log.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger()
-logging.basicConfig(format='[%(asctime)s] %(levelname)-8s [%(filename)s:%(lineno)d] --> %(message)s',
+logging.basicConfig(format='[%(asctime)s] %(levelname)-8s [%(filename)s:%(lineno)d] - %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S',
     level=logging.DEBUG)
 
@@ -13,3 +13,18 @@ except ZeroDivisionError:
     logger.error(f"{test(1,0)}")
 
 test(1, 0)
+
+import json
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        return json.dumps(record.__dict__)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(JsonFormatter())
+logger.addHandler(handler)
