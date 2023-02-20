82c7136fad881016e313a8f1804b78832d4cb4d0
Ashkan Goleh <ashkangoleh@gmail.com>
2023-02-20 14:43:18 +0330

    modified:   CELERY_BEAT/celeryconfig.py

--- CELERY_BEAT/celeryconfig.py
+++ CELERY_BEAT/celeryconfig.py
@@ -14,4 +14,43 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': 30.0,
         # 'args': (16, 16)
     },
+
+    'yearly-task': {
+        'task': 'my_task',
+        # run once a year on Jan 1st
+        'schedule': crontab(minute=0, hour=0, day_of_month=1, month_of_year=1),
+    },
+    'monthly-task': {
+        'task': 'my_task',
+        # run once a month on the 1st
+        'schedule': crontab(minute=0, hour=0, day_of_month=1),
+    },
+    'weekly-task': {
+        'task': 'my_task',
+        # run once a week on Sunday
+        'schedule': crontab(minute=0, hour=0, day_of_week=0),
+    },
+    'daily-task': {
+        'task': 'my_task',
+        'schedule': crontab(minute=0, hour=0),  # run once a day at midnight
+    },
+    'hourly-task': {
+        'task': 'my_task',
+        # run once an hour at the start of the hour
+        'schedule': crontab(minute=0),
+    },
+    'every-15-min-task': {
+        'task': 'my_task',
+        'schedule': crontab(minute='*/15'),  # run every 15 minutes
+    },
+    'weekday-work-hours-task': {
+        'task': 'my_task',
+        # run every weekday from 9am to 5pm
+        'schedule': crontab(minute=0, hour='9-17', day_of_week='1-5'),
+    },
+
 }
+# 'every-15-min-task': {
+#     'task': 'my_task',
+#     'schedule': '*/15 * * * *',  # run every 15 minutes
+# },
