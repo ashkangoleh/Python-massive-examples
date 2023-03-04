741191591000afd8226f093782ca9f088858ae9b
Ashkan Goleh <ashkangoleh@gmail.com>
2023-03-04 10:54:12 +0330

    Qeue sample added

---
+++ PYTHON_EXTRA/Queue.py
@@ -0,0 +1,26 @@
+import multiprocessing
+import time
+
+def run():
+    books = [
+        "pride-and-prejudice.txt",
+        "heart-of-darkness.txt",
+        "frankenstein.txt",
+        "dracula.txt",
+    ]
+    queue = multiprocessing.Queue()
+
+    print("Enqueuing...")
+    for book in books:
+        print(book)
+        queue.put(book)
+        time.sleep(1)
+        
+    print("\nDequeuing...")
+    while not queue.empty():
+        time.sleep(1)
+        print(queue.get())
+
+
+if __name__ == "__main__":
+    run()
